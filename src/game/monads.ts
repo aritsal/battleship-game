9ddabde2e2eced0e435c5596c3def166a3dd0726
art <toritosalgado90@gmail.com>
2023-09-23 14:59:59 -0700

    Brought back monands !!!

--- src/game/monads.ts
+++
@@ -1,7 +0,0 @@
-export type Maybe<T> = T | null | undefined;
-
-export function bindMaybe<A, R>(m: Maybe<A>, func: (a: A) => Maybe<R>): Maybe<R> {
-    if (m !== null || m !== undefined) 
-        return func(m as A);
-    return null;
-}
